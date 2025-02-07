0112c704a80799fa8631fb1c62ece4c2aba9a33a
shiieav <168695016+shiieav@users.noreply.github.com>
2025-02-07 13:29:53 +0800

    index1.php

--- index1.php
+++ index1.php
@@ -17,7 +17,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "<p><strong>Name:</strong> " . $name . "</p>";
     echo "<p><strong>Email:</strong> " . $email . "</p>";
     echo "<p><strong>Contact:</strong> " . $contact . "</p>";
-    echo "<p><strong>Message:</strong> " . nl2br($message) . "</p>"; // nl2br() to preserve new lines in the message
+    echo "<p><strong>Message:</strong> " . nl2br($message) . "</p>";
     echo "</div>";  
 }
 ?>
